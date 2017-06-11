b86cd5eb6cf42c3f52f5148b1cfe2472e2cde338
iastanley <iasvks.craigslist@gmail.com>
2017-06-11 12:53:26 -0700

    fixed searchbar styling and added throttle to search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -15,18 +16,25 @@ class App extends Component {
       selectedVideo: null
     }
     //to show the user some videos on first load
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
     YTSearch({
       key: API_KEY,
-      term: 'surfboards'
+      term: term
     },
     videos => this.setState({ videos, selectedVideo: videos[0] })
     );
   }
 
   render() {
+    //using lodash to throttle videoSearch callback method
+    const videoSearch = _.debounce(term => this.videoSearch(term), 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
