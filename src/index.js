72efd21b178808f566a1792a77533ea422d4aec8
iastanley <iasvks.craigslist@gmail.com>
2017-06-11 10:28:20 -0700

    refactored App to a class componenet

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = process.env.API_KEY;
 
-const App = () => {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    }
+    //to show the user some videos on first load
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    }, function(videos) {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
       <div>
         <SearchBar />
       </div>
-  );
-}
+    );
+  }
+} //end of App component
 
 ReactDOM.render(
   <App/>,
