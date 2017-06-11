7533614fa3492bb8f441c5120a74b003eeb1b2f8
iastanley <iasvks.craigslist@gmail.com>
2017-06-11 12:35:43 -0700

    added ability to select a VideoListItem and update selectedVideo

--- src/index.js
+++ src/index.js
@@ -11,14 +11,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
     //to show the user some videos on first load
     YTSearch({
       key: API_KEY,
       term: 'surfboards'
     },
-    videos => this.setState({ videos })
+    videos => this.setState({ videos, selectedVideo: videos[0] })
     );
   }
 
@@ -26,8 +27,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     );
   }
